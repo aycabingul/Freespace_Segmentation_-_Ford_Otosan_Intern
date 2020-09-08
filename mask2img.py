9874d358cd9b95d2d2d1fc11e78c65eea37027b0
aycabingul <aycaburcu99@gmail.com>
2020-09-09 00:00:59 +0300

    update

--- mask2img.py
+++ mask2img.py
@@ -4,25 +4,26 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+#kütüphaneler import edildi
 import cv2 
 import os
 import numpy as np
 
+#result yolunda bir dosya yoksa eğer o dosya oluşturuldu
 result='/home/aycaburcu/Masaüstü/Ford_Otosan_Intern/data/result'
 if not os.path.exists(result):
     os.mkdir(result)
-    
+#IMG_DIR yolundaki mask'e name'ler okunup bir listeye kaydedildi
 IMG_DIR='/home/aycaburcu/Masaüstü/Ford_Otosan_Intern/data/images'
-os.chdir('/home/aycaburcu/Masaüstü/Ford_Otosan_Intern/data/masks')#geçerli çalışma dizinini verilen yola değiştirir
-masks_name=os.listdir()#ann klasörü içindeki json dosyalarının isimleriyle liste oluşturuldu
+os.chdir('/home/aycaburcu/Masaüstü/Ford_Otosan_Intern/data/masks')
+masks_name=os.listdir()
 
 
 MASK_DIR = '/home/aycaburcu/Masaüstü/Ford_Otosan_Intern/data/masks'
 
 
-for maskname in masks_name:
-    img=cv2.imread(os.path.join(IMG_DIR,maskname[:-4]+'.jpg'))
+for maskname in masks_name:#masks_name listesi'nin tek tek elemanlarına ulaşıldı
+    img=cv2.imread(os.path.join(IMG_DIR,maskname[:-4]+'.jpg'))#maskname sonundan .png kesilerek .jpg eklenip okundu  
     mask=cv2.imread(os.path.join(MASK_DIR,maskname),0)
-    res=cv2.bitwise_or(img,img,mask=mask)
-    cv2.imwrite(os.path.join(result,maskname[:-4]+'.jpg'),res)
+    res=cv2.bitwise_and(img,img,mask=mask)#mask'ın üzerine resim yazdırıldı 
+    cv2.imwrite(os.path.join(result,maskname[:-4]+'.jpg'),res)#yazdırıldı 
